735b5f8c387c0bdfdbf1c420daffdd7d3d77283b
Adam Sibik <adamos135+bitbucket@gmail.com>
2017-03-09 22:25:59 +0100

    Implement Google Oauth logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource)
+    user_path(current_user)
+  end
+
+  # This method is a must when using Devise ONLY with Omniauthable
+  def new_session_path(scope)
+    new_user_session_path
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,18 @@ class User < ApplicationRecord
   has_many :rentals
   has_many :users, through: :rentals
   devise :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.from_omniauth(access_token)
+    user = User.where(uid: access_token['uid'],
+                      provider: access_token['provider'])
+               .first
+
+    unless user
+      user = User.create!(name: access_token.info['name'],
+                          uid: access_token['uid'],
+                          provider: access_token['provider'])
+    end
+
+    user
+  end
 end
