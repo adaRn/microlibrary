ddc8dc222f148734a922ea0480dabb82994eef1b
Adam Sibik <adamos135+bitbucket@gmail.com>
2017-03-09 23:45:09 +0100

    Add book cover image helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def book_cover_image(book)
+    image_tag(
+      "http://images.amazon.com/images/P/#{book.isbn.gsub(/[^0-9,.]/, '')}.01.ZTZZZZZZ.jpg",
+      alt: "#{book.title} cover"
+    )
+  end
 end
