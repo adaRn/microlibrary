c5105d6be4b0c1cf2b4f55b2cf511187f1cd3491
Adam Sibik <adamos135+bitbucket@gmail.com>
2017-03-09 15:05:38 +0100

    Add Rentals controller and view

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170308090120) do
+ActiveRecord::Schema.define(version: 20170308091418) do
 
   create_table "books", force: :cascade do |t|
     t.text     "title"
@@ -18,6 +18,17 @@ ActiveRecord::Schema.define(version: 20170308090120) do
     t.string   "isbn"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "rentals", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "book_id"
+    t.date     "start_date"
+    t.date     "due_date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["book_id"], name: "index_rentals_on_book_id"
+    t.index ["user_id"], name: "index_rentals_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
