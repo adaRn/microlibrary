3de548af4d3319a342c287cd4772c5669d487bb4
Adam Sibik <adamos135+bitbucket@gmail.com>
2017-03-09 23:08:03 +0100

    Fix test bugs (not testing for href parameter)

--- test/integration/user_flow_test.rb
+++ test/integration/user_flow_test.rb
@@ -3,20 +3,20 @@ require 'test_helper'
 class UserFlowTest < ActionDispatch::IntegrationTest
   include Devise::Test::IntegrationHelpers
 
-  test 'can see the link to Google login' do
+  test 'can see one link to Google login if not logged in' do
     get '/'
-    assert_select 'a', href: '/users/auth/google_oauth2'
+    assert_select 'a[href=?]', '/users/auth/google_oauth2', count: 1
   end
 
   test 'cannot see the link to Google login if logged in' do
     sign_in create(:user)
     get '/'
-    assert_select 'a', href: '/users/auth/google_oauth2', count: 0
+    assert_select 'a[href=?]', '/users/auth/google_oauth2', count: 0
   end
 
-  test 'can see logout link if logged in' do
+  test 'can see one logout link if signed in' do
     sign_in create(:user)
     get '/'
-    assert_select 'a', href: '/sign_out', count: 1
+    assert_select 'a[href=?]', 'sign_out', count: 1
   end
 end
