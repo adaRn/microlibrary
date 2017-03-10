ce59370c8857a4c06bc2cf925af328fa6df1064e
Adam Sibik <adamos135+bitbucket@gmail.com>
2017-03-10 20:57:18 +0100

    Set the start date and due date automatically

--- app/models/rental.rb
+++ app/models/rental.rb
@@ -1,8 +1,16 @@
 class Rental < ApplicationRecord
   belongs_to :user
   belongs_to :book
+  before_create :set_start_and_due_date
 
   def finished?
     !finish_date.nil?
   end
+
+  private
+
+    def set_start_and_due_date
+      self.start_date = Date.today
+      self.due_date = 14.days.from_now
+    end
 end
--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -5,4 +5,15 @@ class RentalTest < ActiveSupport::TestCase
     assert_not rentals(:started).finished?
     assert rentals(:finished).finished?
   end
+
+  test 'should set the start date of rental to today automatically' do
+    rental = Rental.create(user: users(:one), book: books(:never_rented))
+    assert_equal Date.today, rental.start_date
+    assert_not_equal rental.start_date, rental.due_date
+  end
+
+  test 'should set the due date of rental to some future date automatically' do
+    rental = Rental.create(user: users(:one), book: books(:never_rented))
+    assert rental.start_date < rental.due_date
+  end
 end
