6d26a5e100184cb97fab855c2b663c13486e100d
Adam Sibik <adamos135+bitbucket@gmail.com>
2017-03-10 17:58:07 +0100

    Change the default path after signing in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(resource)
-    user_path(current_user)
+    books_path
   end
 
   # This method is a must when using Devise ONLY with Omniauthable
