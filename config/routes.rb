d68f645b0e4503e8696cd446065469364b61617b
Adam Sibik <adamos135+bitbucket@gmail.com>
2017-03-10 18:01:18 +0100

    Fix sign in path routing bug

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
   devise_for :users,
              controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   devise_scope :user do
-    get 'sign_in', to: 'home#static_pages', as: 'new_user_session'
+    get 'sign_in', to: 'static_pages#home', as: 'new_user_session'
     get 'sign_out', to: 'devise/sessions#destroy', as: 'destroy_user_session'
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
