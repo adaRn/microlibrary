cdefa7537c1dd2f89f8a519ecef95540e910c259
Adam Sibik <adamos135+bitbucket@gmail.com>
2017-03-11 01:22:36 +0100

    Validate presence of a Book title and author

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,7 @@
 class Book < ApplicationRecord
   has_many :rentals
   has_many :users, through: :rentals
+  validates :title, :author, presence: true
 
   def rented?
     return rentals.where(finish_date: nil).any?
